35a28e6467a19a3f51c20065fb135d81e145411c
Timilehin Arigbede <Harriemannie@gmail.com>
2022-05-10 16:32:46 +0100

    Create Add Todo Form

--- app.py
+++ app.py
@@ -8,6 +8,10 @@ app.config['SECRET_KEY'] = 'mysecret'
 
 todos = ["Learn Flask", "Set up venv", "Build cool app"]
 
+class TodoForm(FlaskForm):
+  todo = StringField("Todo")
+  submit = SubmitField("Add Todo")
+
 @app.route('/', methods=["GET", "POST"])
 def index():
-  return render_template("index.html", todos=todos)
+  return render_template("index.html", todos=todos, template_form=TodoForm())
